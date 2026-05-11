2a362d5864b1330adc6c96acaedb05bcff5d105b
gigiceva-code <gigi.ceva@gmail.com>
2026-05-11 12:22:12 +0200

    Update check-and-create.js

--- api/check-and-create.js
+++ api/check-and-create.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const { nomeTag } = req.body;
   const AIRTABLE_TOKEN = process.env.AIRTABLE_TOKEN;
   const BASE_ID = process.env.AIRTABLE_BASE_ID;
-  const TABLE_ID = 'tblywlZwSsFKWsQn4'; 
+  const TABLE_ID = process.env.AIRTABLE_TABLE_ID; 
 
   try {
     // 1. Controllo se esiste
